fce39da523b1ad5811f61e5fd6aedf6e867f6637
bloomlj <kolocc@live.com>
2015-01-09 02:09:45 +0800

    add report url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var mp = require('wechat-mp')("vchat")
-var app = require('express')()
+var mp = require('wechat-mp')("vchat");
+var express = require('express');
+var app = express();
 
 //mysql
 var knex = require('knex')({
@@ -12,8 +13,36 @@ var knex = require('knex')({
   }
 });
 
+app.use('/static', express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
     res.send("hahahhaha");
+});
+
+app.get('/report/all',function(req,res){
+    
+        knex.raw('select  org.name,sum(score) as totalscore  from score left join org on score.org_id = org.id group by org_id order by totalscore desc').then(function(resp) {
+
+            var reportstring = "";
+            for(i in resp[0]){
+                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"<br/>";
+            }
+            res.send(reportstring);
+        });
+    
+    
+    
+});
+
+app.get('/report/me',function(req,res){
+            knex.raw("select org.name,score from score left join org on org.id = score.org_id where  uid = 'oMR8gsycPJgCn2PH0RVAvlPd3oCc'  order by score desc ").then(function(resp) {
+
+            var reportstring = "";
+            for(i in resp[0]){
+                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].score+"<br/>";
+            }
+            res.send(reportstring);
+        });
 });
 
 app.use('/wechat', mp.start())
@@ -27,21 +56,20 @@ app.post('/wechat', function(req, res, next) {
     console.log("msg log success")
  });
     
- console.log(req.body)
  
- //msg define
- var msgNews = { 
-  msgType: 'news',
-  content: [{
-    title: '打分规则',
-    url: 'http://...',
-    picUrl: 'http://...'
-  }, {
-    title: 'news 2',
-    url: 'http://...',
-    picUrl: 'http://...'
-  }]
-}
+// //msg define
+// var msgNews = { 
+//  msgType: 'news',
+//  content: [{
+//    title: '打分规则',
+//    url: 'http://...',
+//    picUrl: 'http://...'
+//  }, {
+//    title: 'news 2',
+//    url: 'http://...',
+//    picUrl: 'http://...'
+//  }]
+//}
  
  var msgDf = {
         msgType: 'text',
@@ -60,24 +88,37 @@ app.post('/wechat', function(req, res, next) {
     msgType: 'text',
     content: '命令执行完毕，评分成功。'
   };
+    
+    
+ var msgReportAll = { 
+  msgType: 'news',
+  content: [{
+    title: '全部评分结果报告',
+    url: 'http://vchat.ngrok.com/report/all',
+    picUrl: 'http://vchat.ngrok.com/static/report.png'
+  }]
+}
+
+  var msgReportMe = { 
+  msgType: 'news',
+  content: [{
+    title: '我的评分结果报告',
+    url: 'http://vchat.ngrok.com/report/me',
+    picUrl: 'http://vchat.ngrok.com/static/report.png'
+  }]
+}
+    
 //logic
  if(req.body.raw.MsgType == 'text'){
      if(req.body.raw.Content.toLowerCase() == 'df') res.body = msgDf;
-     else
-        if(req.body.raw.Content.toLowerCase().indexOf('a') == 0){
+     else if(req.body.raw.Content.toLowerCase().indexOf('a') == 0){
             var msg_full = req.body.raw.Content.toLowerCase();
             var orgstring = msg_full.substring(0,msg_full.indexOf(' '));
             var orgid = orgstring.substring(1);
             
-            var scorestring = msg_full.substring(msg_full.lastIndexOf(' ')+1);
-            
+            var scorestring = msg_full.substring(msg_full.lastIndexOf(' ')+1);       
             var total = 0;
-            console.log(msg_full);
-            console.log("["+orgstring+"]");
-             console.log("["+scorestring+"]");
             if(scorestring.indexOf('s') == 0){
-                console.log("1111111111");
-                console.log(scorestring.substring(1));
                 total = parseFloat(scorestring.substring(1));
             }else{
                 var scorearray = scorestring.split(",");
@@ -86,21 +127,34 @@ app.post('/wechat', function(req, res, next) {
                     total=total + parseFloat(scorearray[x]);
                   }
             }
-            console.log(total);
-            
-            console.log(orgid);
-                 knex('score')
-                 .insert([{org_id:orgid,uid:req.body.raw.FromUserName,score:total,msg_id:req.body.raw.MsgId}])
+                 knex('score')                 .insert([{org_id:orgid,uid:req.body.raw.FromUserName,score:total,msg_id:req.body.raw.MsgId}])
                  .then(function(ret){
                     console.log("db save success")
                     
                  });
             res.body = msgSuccessCmd; 
         } 
-        else
-            if()
+        else if(req.body.raw.Content.toLowerCase().indexOf('report_all') == 0){
+                res.body = msgReportAll; 
+                knex.raw('select  org.name,sum(score) as totalscore  from score left join org on score.org_id = org.id group by org_id order by totalscore desc').then(function(resp) {
+                     //console.log(res);
+                    //res.body = "xx";
+                        var reportstring = "";
+                        for(i in resp[0]){
+                           // console.log(resp[0][i]);
+                            reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"\n";
+                        }
+                          var reportmsg = {
+                            msgType: 'text',
+                            content: reportstring
+                          };
+                        res.body = reportmsg;
+                    });
+                     
+            }else if(req.body.raw.Content.toLowerCase().indexOf('report_me') == 0){
+                res.body = msgReportMe; 
             
-            res.body = msgDefault;
+            } else res.body = msgDefault;
  }else{
      res.body = msgError; 
  }
@@ -116,6 +170,11 @@ app.post('/wechat', function(req, res, next) {
 //  }
   next()
 }, mp.end())
+
+var responseMsg = function(res,msg){
+    res.body = msg;
+}
+
 
 app.listen(1234,function(){
     console.log("running at 1234.");
