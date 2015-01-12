abcf69aeb02e9c747da331733951a0e945bdacb2
bloomlj <kollocc@live.com>
2015-01-13 01:55:34 +0800

    add files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var mp = require('wechat-mp')("vchat");
 var express = require('express');
+var hbs = require('hbs');
 var app = express();
+
+app.set('view engine', 'hbs');
 
 //mysql
 var knex = require('knex')({
@@ -23,11 +26,13 @@ app.get('/report/all',function(req,res){
     
         knex.raw('select  org.name,sum(score) as totalscore  from score left join org on score.org_id = org.id group by org_id order by totalscore desc').then(function(resp) {
 
-            var reportstring = "";
-            for(i in resp[0]){
-                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"<br/>";
-            }
-            res.send(reportstring);
+//            var reportstring = "";
+//            for(i in resp[0]){
+//                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"<br/>";
+//            }
+            //res.send(reportstring);
+            res.render('report', {'data':resp[0]});
+            
         });
     
     
@@ -37,11 +42,12 @@ app.get('/report/all',function(req,res){
 app.get('/report/me',function(req,res){
             knex.raw("select org.name,score from score left join org on org.id = score.org_id where  uid = 'oMR8gsycPJgCn2PH0RVAvlPd3oCc'  order by score desc ").then(function(resp) {
 
-            var reportstring = "";
-            for(i in resp[0]){
-                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].score+"<br/>";
-            }
-            res.send(reportstring);
+//            var reportstring = "";
+//            for(i in resp[0]){
+//                reportstring = reportstring+resp[0][i].name+":"+resp[0][i].score+"<br/>";
+//            }
+//            res.send(reportstring);
+            res.render('reportme', {'data':resp[0]});
         });
 });
 
@@ -75,6 +81,10 @@ app.post('/wechat', function(req, res, next) {
         msgType: 'text',
         content: '您好！欢迎进入打分模式。\n请按照使用说明输入打分单位和分值。\n指令：#id,index1,index2,index3。\nid：单位编号index1：指标一、工作完成情况 index2：指标二、内部管理 index3：指标三、领导班子状况'
       };
+   var msgBD = {
+    msgType: 'text',
+    content: '绑定成功。'
+  };   
  var msgDefault = {
     msgType: 'text',
     content: '您发的指令我不认识。'
@@ -88,7 +98,7 @@ app.post('/wechat', function(req, res, next) {
     msgType: 'text',
     content: '命令执行完毕，评分成功。'
   };
-    
+
     
  var msgReportAll = { 
   msgType: 'news',
@@ -110,7 +120,35 @@ app.post('/wechat', function(req, res, next) {
     
 //logic
  if(req.body.raw.MsgType == 'text'){
+     //打分提示
      if(req.body.raw.Content.toLowerCase() == 'df') res.body = msgDf;
+     //绑定code
+     else if(req.body.raw.Content.toLowerCase().indexOf('bd') == 0){
+        var userCode = req.body.raw.Content.toLowerCase().substring(2);
+         knex('user').count('code as count').where('uid',req.body.raw.FromUserName).then(function(ret){
+            //console.log(ret[0].count);
+             if(ret[0].count > 0){
+                  knex('user')
+                  .where('uid',req.body.raw.FromUserName)
+                 .update({code:userCode})
+                 .then(function(ret){
+                    //console.log(ret);
+                    console.log("BD update success by:"+req.body.raw.FromUserName)
+                 });
+             }
+             else{
+                 knex('user')
+                 .insert([{uid:req.body.raw.FromUserName,code:userCode}])
+                 .then(function(ret){
+                    //console.log(ret);
+                    console.log("BD new success by:"+req.body.raw.FromUserName)
+                 });
+             }
+         });
+
+        res.body = msgBD;
+     }
+     //打分指令
      else if(req.body.raw.Content.toLowerCase().indexOf('a') == 0){
             var msg_full = req.body.raw.Content.toLowerCase();
             var orgstring = msg_full.substring(0,msg_full.indexOf(' '));
@@ -127,31 +165,49 @@ app.post('/wechat', function(req, res, next) {
                     total=total + parseFloat(scorearray[x]);
                   }
             }
-                 knex('score')                 .insert([{org_id:orgid,uid:req.body.raw.FromUserName,score:total,msg_id:req.body.raw.MsgId}])
+             knex('score').count('uid as count').where({org_id:orgid,uid:req.body.raw.FromUserName}).then(function(ret){
+                 console.log(ret[0].count);
+                  if(ret[0].count > 0){
+                    knex('score')
+                    .where({org_id:orgid,uid:req.body.raw.FromUserName})
+                    .update({score:total,msg_id:req.body.raw.MsgId})
+                    .then(function(ret){
+                        console.log("df update success by:"+req.body.raw.FromUserName)
+                    });
+                  }else{
+                    knex('score')                                   .insert([{org_id:orgid,uid:req.body.raw.FromUserName,score:total,msg_id:req.body.raw.MsgId}])
                  .then(function(ret){
-                    console.log("db save success")
-                    
+                    console.log("df new success by:"+req.body.raw.FromUserName)
                  });
+                    
+                  }
+             
+             
+             });
+         
+
             res.body = msgSuccessCmd; 
         } 
+       //全部报表
         else if(req.body.raw.Content.toLowerCase().indexOf('report_all') == 0){
                 res.body = msgReportAll; 
-                knex.raw('select  org.name,sum(score) as totalscore  from score left join org on score.org_id = org.id group by org_id order by totalscore desc').then(function(resp) {
-                     //console.log(res);
-                    //res.body = "xx";
-                        var reportstring = "";
-                        for(i in resp[0]){
-                           // console.log(resp[0][i]);
-                            reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"\n";
-                        }
-                          var reportmsg = {
-                            msgType: 'text',
-                            content: reportstring
-                          };
-                        res.body = reportmsg;
-                    });
-                     
-            }else if(req.body.raw.Content.toLowerCase().indexOf('report_me') == 0){
+//                knex.raw('select  org.name,sum(score) as totalscore  from score left join org on score.org_id = org.id group by org_id order by totalscore desc').then(function(resp) {
+//                     //console.log(res);
+//                    //res.body = "xx";
+//                        var reportstring = "";
+//                        for(i in resp[0]){
+//                           // console.log(resp[0][i]);
+//                            reportstring = reportstring+resp[0][i].name+":"+resp[0][i].totalscore+"\n";
+//                        }
+//                          var reportmsg = {
+//                            msgType: 'text',
+//                            content: reportstring
+//                          };
+//                        res.body = reportmsg;
+//                    });                
+            }
+        //个人报表
+        else if(req.body.raw.Content.toLowerCase().indexOf('report_me') == 0){
                 res.body = msgReportMe; 
             
             } else res.body = msgDefault;
